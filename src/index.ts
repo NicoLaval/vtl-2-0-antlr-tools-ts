05c60b37ac752d3ef3e7b2ea89bd1b9bf53af0d6
NicoLaval <nicolas.laval@insee.fr>
2021-05-07 12:16:43 +0200

    Add grammar files

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export { myFunction } from "./myFunction";
-export { myObject } from "./myObject";
+export { VtlLexer as Lexer } from "./generated/VtlLexer";
+export { VtlParser as Parser } from "./generated/VtlParser";
+// @ts-ignore
+export { default as Grammar } from "raw-loader!./source/Vtl.g4";
