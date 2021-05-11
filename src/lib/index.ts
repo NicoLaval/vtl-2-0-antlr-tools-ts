7fc048904a8f9cff2945c57b4c13b64a7424db27
NicoLaval <nicolas.laval@insee.fr>
2021-05-11 15:33:41 +0200

    Update

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,7 @@
 export { VtlLexer as Lexer } from "./generated/VtlLexer";
 export { VtlParser as Parser } from "./generated/VtlParser";
 export { default as grammar } from "./generated/Vtl.g4";
+
+const initialRule = "start";
+
+export { initialRule };
