6074f9f68a01ba8d65031a0dcc7f07c10a6bd51b
NicoLaval <nicolas.laval@insee.fr>
2021-05-11 14:25:27 +0200

    Refactor

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,3 @@
 export { VtlLexer as Lexer } from "./generated/VtlLexer";
 export { VtlParser as Parser } from "./generated/VtlParser";
-export { default as Grammar } from "./generated/Vtl.g4";
+export { default as grammar } from "./generated/Vtl.g4";
