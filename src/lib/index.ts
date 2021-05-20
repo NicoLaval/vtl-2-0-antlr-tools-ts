d494c1ac8f69134c6a8cd1b5ca3837f9532b103e
NicoLaval <nicolas.laval@insee.fr>
2021-05-20 17:22:32 +0200

    Remove visitor

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,5 @@
 export { VtlLexer as Lexer } from "./generated/VtlLexer";
 export { VtlParser as Parser } from "./generated/VtlParser";
-export { VtlVisitor as Visitor } from "./generated/VtlVisitor";
 export { default as grammar } from "./generated/Vtl.g4";
 
 const initialRule = "start";
