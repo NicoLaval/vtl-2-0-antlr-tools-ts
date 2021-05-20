2795ffc392d2171577f0a4231390518657a36a71
NicoLaval <nicolas.laval@insee.fr>
2021-05-20 15:43:20 +0200

    Add visitor

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 export { VtlLexer as Lexer } from "./generated/VtlLexer";
 export { VtlParser as Parser } from "./generated/VtlParser";
+export { VtlVisitor as Visitor } from "./generated/VtlVisitor";
 export { default as grammar } from "./generated/Vtl.g4";
 
 const initialRule = "start";
